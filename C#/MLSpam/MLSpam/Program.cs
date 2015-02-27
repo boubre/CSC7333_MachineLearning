95d3ab99f6b37e9a4992490c0a5d058961237dc7
Brandon Oubre <br.oubre@yahoo.com>
2015-02-27 15:15:57 -0600

    Created a new C# project.

---
+++ C#/MLSpam/MLSpam/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLSpam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("CSC 7333 Machine Learning Project");
+
+            Console.Out.WriteLine("Execution Complete.");
+            Console.In.ReadLine(); //Wait for enter to be pressed before exiting
+        }
+    }
+}
