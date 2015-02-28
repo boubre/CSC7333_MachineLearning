ae08b9adc17e593ecbec59e611abf141588ad1ef
Brandon Oubre <br.oubre@yahoo.com>
2015-02-27 18:35:36 -0600

    Added Java project with Weka library for C4.5.

---
+++ Java/src/csc7333/Main.java
@@ -0,0 +1,15 @@
+package csc7333;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("CSC 7333 Machine Learning Project");
+		
+		uciSpam();
+		
+		System.out.println("Execution Complete.");
+	}
+	
+	private static void uciSpam() {
+		
+	}
+}
